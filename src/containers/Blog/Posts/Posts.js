d25dbf14cf5ded66336b0625d21d07ce94c7d87c
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2019-10-23 19:47:04 +0200

    adding nested routes

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
+import { Route } from "react-router-dom";
 import axios from "../../../axios";
 import Post from "../../../components/Post/Post";
+import FullPost from "../FullPost/FullPost";
 
 import "./Posts.css";
 
@@ -31,14 +33,14 @@ class Posts extends Component {
   }
 
   postSelectedHandler = id => {
-    this.props.history.push({ pathname: "/" + id });
+    this.props.history.push({ pathname: "/posts/" + id });
     // this.props.history.push("/" + id);
   };
   render() {
     let posts = <p style={{ textAlign: "center" }}>Something went wrong!</p>;
     if (!this.state.error) {
       posts = this.state.posts.map(post => (
-        // <Link to={"/" + post.id}>
+        // <Link to={"/posts" + post.id}>
         <Post
           key={post.id}
           title={post.title}
@@ -48,7 +50,16 @@ class Posts extends Component {
         // </Link>
       ));
     }
-    return <section className="Posts">{posts}</section>;
+    return (
+      <div>
+        <section className="Posts">{posts}</section>
+        <Route
+          path={this.props.match.url + "/:id"}
+          exact
+          component={FullPost}
+        />
+      </div>
+    );
   }
 }
 
